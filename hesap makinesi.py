b649f302d0af9e439c597afe64a46c25d71f5e7a
frontenddd <99826577+frontenddd@users.noreply.github.com>
2023-03-02 13:06:52 +0300

    Add files via upload

---
+++ hesap makinesi.py
@@ -0,0 +1,19 @@
+s1=int(input(""))
+print(s1)
+
+islem=input("")
+print(s1,islem)
+
+s2=int(input(""))
+
+if islem=="+":
+    sonuc=(s1)+(s2)
+elif islem=="-": 
+    sonuc=(s1)-(s2)
+elif islem=="*": 
+    sonuc=(s1)*(s2)
+elif islem=="/": 
+    sonuc=(s1)/(s2)        
+else: print("hatalı islem girdiniz")
+
+print(s1,islem,s2,"=",sonuc)  
